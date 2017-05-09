b021fdf791e99ce8ffb2639f99ea03cef1078604
Jeffrey Brown <Jeffrey Brown>
2017-05-09 00:52:33 -0400

    working-routes

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+var friends = [
+	{
+	  "name":"Tiffany",
+	  "photo":"../../../images/tiffany.JPG",
+	  "scores":[
+	      5,
+	      1,
+	      4,
+	      4,
+	      5,
+	      1,
+	      2,
+	      5,
+	      4,
+	      1
+	    ]
+	}
+]
+
+module.exports = friends;
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -5,15 +5,7 @@ module.exports = function(app) {
 		res.sendFile(path.join(__dirname, "../public/home.html"));
 	});
 
-	app.get("/tables", function (req, res){
-		res.sendFile(path.join(__dirname, "../public/tables.html"));
+	app.get("/survey", function (req, res){
+		res.sendFile(path.join(__dirname, "../public/survey.html"));
 	});
-
-	app.get("/reserve", function (req, res){
-		res.sendFile(path.join(__dirname, "../public/reserve.html"));
-	});
-
-
-
-
 }
